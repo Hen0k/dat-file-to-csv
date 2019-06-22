dc65bf7ec4c17a18c1112fcaacbe2beb56e7a23a
Dawit Abate <dawitabate2@gmail.com>
2019-06-22 15:40:38 +0300

    Simplified methods in helpers

--- helpers.py
+++ helpers.py
@@ -1,48 +1,35 @@
-import math
+from datetime import timedelta
 import csv
+
 def to_decimal(ftime):
     """This function converts hours 
     that are in the for hh:mm:sec 
     into fractions of the day"""
 
-    hours, minutes, seconds = ftime.split(":")
-    hours = int(hours)
-    minutes = int(minutes)
-    seconds = int(seconds)
+    duration = timedelta(**dict(zip(['hours', 'minutes', 'seconds'],
+                                    map(int, ftime.split(':')))))
+    return duration.total_seconds()/(24*60*60.0)
 
-    hours = hours/24.0
-    #print (hours)
-    minutes = minutes/(24 * 60.0)
-    #print (minutes)
-    seconds = seconds / (24*60*60.0)
-    totalInDec = hours+minutes+seconds
-
-    return totalInDec    
-
-print (to_decimal("10:47:00"))
 
 def to_ftime(dec_time):
     """This function converts hours 
     that are in decimal forms(what fraction
     of the day they are to) to a fromated string
     like hh:mm:ss"""
-    temp = dec_time * 24
-    hours = math.floor(temp)
-    tempm = (temp - hours) * 60
-    minutes = math.floor(tempm)
-    temps = (tempm - minutes) * 60
-    seconds = math.floor(temps)
 
-    return ( str(int(hours)).zfill(2) + ":" + str(int(minutes)).zfill(2) + ":" + str(int(seconds)).zfill(2) )
+    dec_time *= 24*60*60
+    hours, remainder = divmod(dec_time, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return '{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds))
+
+
 def write_to_csv(data, file_name, row_names):
-    """
-        This Function writes an arry of dictionaries
-        to a csv file using the csv modules DIctwriter function
-    """
+    """This Function writes an array of dictionaries
+    to a csv file using the csv modules DictWriter function"""
+
     f = open("csv/" + file_name + ".csv", "w")
     writer = csv.DictWriter(f, fieldnames=row_names)
     writer.writeheader()
     writer.writerows(data)
     f.close()
-# print (to_ftime(0.44930555555555557))
-# print (to_ftime(0.189583))
+
